94fd9b82ab3548f5f51507c428f7ee61563dda04
M3NGSZE <mengse.thun13@gmail.com>
2025-09-09 22:32:16 +0700

    prepare strucute

--- app/src/main/java/com/example/a10_thun_mengse_pvh_homework_003/MainActivity.kt
+++ app/src/main/java/com/example/a10_thun_mengse_pvh_homework_003/MainActivity.kt
@@ -18,30 +18,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            _10_THUN_MENGSE_PVH_HOMEWORK_003Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    _10_THUN_MENGSE_PVH_HOMEWORK_003Theme {
-        Greeting("Android")
     }
 }
