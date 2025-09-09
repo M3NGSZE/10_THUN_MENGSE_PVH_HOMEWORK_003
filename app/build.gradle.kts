ec36c1cc97e402439f5e6b2dba602b2d48791967
M3NGSZE <mengse.thun13@gmail.com>
2025-09-09 23:14:44 +0700

    sertopit dependecy

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.devtools.ksp") version "1.9.24-1.0.20"
 }
 
 android {
@@ -56,4 +57,22 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation("androidx.room:room-runtime:2.6.1")
+
+    implementation("androidx.room:room-ktx:2.6.1")
+
+
+    implementation("androidx.room:room-paging:2.6.1")
+
+    ksp("androidx.room:room-compiler:2.6.1")
+
+//    implementation(libs.androidx.lifecycle.livedata.ktx)
+//    implementation(libs.androidx.runtime.livedata)
+//
+//    val room_version = "2.7.2"
+//
+//    implementation("androidx.room:room-runtime:$room_version")
+//    ksp("androidx.room:room-compiler:$room_version")
+//    implementation("androidx.room:room-ktx:${room_version}")
 }
