cd210b0a9a390d13975fdf027e1f674e0bb85abd
M3NGSZE <mengse.thun13@gmail.com>
2025-09-10 20:30:26 +0700

    form pop up no interact

--- app/src/main/java/com/example/a10_thun_mengse_pvh_homework_003/ui/components/PopUpDialog.kt
+++ app/src/main/java/com/example/a10_thun_mengse_pvh_homework_003/ui/components/PopUpDialog.kt
@@ -1,23 +1,20 @@
 package com.example.a10_thun_mengse_pvh_homework_003.ui.components
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -26,35 +23,8 @@ import androidx.compose.ui.unit.sp
 @Composable
 fun PopUpDialog(){
     var title by remember { mutableStateOf("") }
-
-//    AlertDialog(
-//        onDismissRequest = {},
-//        title = {},
-//        modifier = TODO(),
-//
-//    )
-//        Column (
-//            modifier = Modifier
-//                .clip(RoundedCornerShape(16.dp))
-//                .height(250.dp)
-//                .padding(16.dp)
-//        ){
-//            Text(
-//                text = "Create New Note",
-//                fontSize = 20.sp,
-//                fontWeight = FontWeight.Medium
-//            )
-//
-//            Spacer(modifier = Modifier.height(16.dp))
-//
-//
-//            TextField(
-//                value = title,
-//                onValueChange = {it -> title = it},
-//                label = {Text("Title")},
-//            )
-//        }
-//    }
+    var note by remember { mutableStateOf("") }
+    val isSave = if (title.isBlank() || note.isBlank()) false else true
 
     AlertDialog(
         title = {
@@ -65,24 +35,63 @@ fun PopUpDialog(){
             )
         },
         text =  {
-            TextField(
-                value = title,
-                onValueChange = {it -> title = it},
-                label = {Text("Title")},
-            )
+            Column {
+                Column {
+                    MyTextField("Title"){}
+
+                    Spacer(modifier = Modifier.height(15.dp))
+
+                    MyTextField("Note"){}
+                }
+            }
+
+
         },
         onDismissRequest ={},
         confirmButton = {
-            Text(
-                text = "Save",
-                modifier = Modifier.clickable{
-
-                }
-            )
+            MyButton("Save", isSave)
         },
 
-        dismissButton =  {Text("kl")},
-
-
+        dismissButton =  {
+            MyButton("Cancel",)
+        },
     )
+}
+
+
+@Composable
+fun MyTextField(
+    title: String,
+    setValue: (String) -> Unit
+){
+    var value by remember { mutableStateOf("") }
+    setValue(value)
+
+    OutlinedTextField(
+        value = value,
+        onValueChange = {it -> value = it.trim()},
+        label = { Text(text = title) },
+    )
+}
+
+@Composable
+fun MyButton(
+    title: String,
+    onOff: Boolean = true
+){
+    Button(
+        modifier = Modifier
+            .width(95.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Transparent,
+            contentColor = Color.Blue,
+            disabledContainerColor = Color.Transparent
+        ),
+        enabled = onOff,
+        onClick = {
+
+        }
+    ) {
+        Text(text = title)
+    }
 }
